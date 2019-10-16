39d4167475cfa6c81f2870aa28b3d613ec6bc570
Cristian Pintor <cpintor@hawk.iit.edu>
2019-10-15 22:41:08 -0500

    About the program

---
+++ license_exam.py
@@ -0,0 +1,8 @@
+"""
+Author: Cristian Pintor
+This program stores the correct answers in a list and then asks
+the user to enter the answers for the 20 questions. The program
+then outputs a fail or pass result based on the entries.
+"""
+
+ans = []
